b81389e0f3b48a241b1d7e8e23fe861b45353707
AlexPerezTimoneda <152508131+AlexPerezTimoneda@users.noreply.github.com>
2025-05-14 19:18:08 +0200

    Add files via upload

---
+++ bin/hello-cdk.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { HelloCdkStack } from '../lib/hello-cdk-stack';
+
+const app = new cdk.App();
+new HelloCdkStack(app, 'HelloCdkStack', {
+env: { account: '971422716475', region: 'eu-south-2' },
+
+  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+});
---
+++ lib/hello-cdk-stack.ts
@@ -0,0 +1,35 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+export class HelloCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const myFunction = new lambda.Function(this, "HelloWorldFunction", {
+      runtime: lambda.Runtime.NODEJS_20_X, // Provide any supported Node.js runtime
+      handler: "index.handler",
+      code: lambda.Code.fromInline(`
+        exports.handler = async function(event) {
+          return {
+            statusCode: 200,
+            body: JSON.stringify('Hello World!'),
+          };
+        };
+      `),
+    });
+
+   // Crear API Gateway REST API
+    const api = new apigateway. LambdaRestApi(this, "HelloApi", {
+    handler: myFunction,
+    proxy: true, // permite enrutar todos los métodos a Lambda
+    });
+
+    // Output con la URL de la API
+    new cdk.CfnOutput(this, "ApiUrl", {
+      value: api.url,
+    });
+    }
+
+  }
+
